25792b31fb604e6d04957030c15b8a9f5726a269
salmantariq01 <49016072+salmantariq01@users.noreply.github.com>
2021-12-24 13:15:42 +0500

    created model -a

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products','App\Http\Controllers\ProductController');
+Route::group(['prefix'=>'products'],function(){
+    Route::apiResource('/{product}/reviews','ReviewController');
 });
